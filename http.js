ddcb46f21a38dfe74c5c7f84f5d0abb5ec80a4df
David <davidesparzaj22@gmail.com>
2025-03-20 19:47:09 -0500

    feat(http.js): add logic for the readFile fn

--- http.js
+++ http.js
@@ -1,13 +1,35 @@
 const http = require("node:http");
+const fs= require('node:fs')
 
 const desiredPort = process.env.PORT ?? 3000;
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
+const processRequest = (req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.end("Welcome to my homepage");
+  } else if (req.url === '/image.jpg') {
+    res.setHeader("Content-Type", "image/jpg")
+    fs.readFile("./image.jpg", (err, data)=> {
+        if (err) {
+            res.statusCode = 500
+            res.end('<h1> 500 Internal Server Error </h1>')
+        }else{
+            res.setHeader("Content-Type", "image/jpg")
+            res.end(data)
+        }
+    })
+  } 
+  else if (req.url === "/contact") {
+    res.statusCode = 200;
+    res.end("<h1>Contact</h1>");
+  } else {
+    res.status = 404;
+    res.end("<h1> 404 </h1>");
+  }
+};
 
-  console.log("request received");
-  res.end("Hola mundo");
-});
+const server = http.createServer(processRequest);
 
 server.listen(desiredPort, () => {
   console.log(`server listening on port http://localhost:${desiredPort}`);
