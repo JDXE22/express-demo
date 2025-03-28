6858d26d5577e1da0344198d796cf0a23f9a4dce
David <davidesparzaj22@gmail.com>
2025-03-28 15:06:39 -0500

    refactor(express-demo/api-rest): modularize all routes into moviesRouter.js and import in app.js

--- API-REST/app.js
+++ API-REST/app.js
@@ -1,8 +1,7 @@
 import express, { json } from "express";
-import movies from "./movies.json" with {type:"json"};
-import { randomUUID } from "node:crypto";
 import cors from "cors";
-import { validateMovie, validatePartialMovie } from "./schemas/movies.js";
+import { moviesRouter } from "./routes/moviesRouter.js";
+moviesRouter
 
 const app = express();
 
@@ -13,18 +12,18 @@ app.use(
         "http://localhost:8080",
         "http://localhost:3000",
         "https://movies.com",
-        "https://localhost:1"
+        "https://localhost:1",
       ];
-      
+
       if (ACCEPTED_ORIGINS.includes(origin)) {
-        return callback(null,true)
+        return callback(null, true);
       }
 
       if (!origin) {
-        return callback(null,true)
+        return callback(null, true);
       }
 
-      return callback(new Error("Not allowed by CORS"))
+      return callback(new Error("Not allowed by CORS"));
     },
   })
 );
@@ -32,81 +31,7 @@ app.use(
 app.use(json());
 app.disable("x-powered-by");
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
-});
-
-app.get("/movies", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  const { genre } = req.query;
-  if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
-    );
-    return res.json(filteredMovies);
-  }
-
-  res.json(movies);
-});
-
-app.get("/movies/:id", (req, res) => {
-  const { id } = req.params;
-  const movie = movies.find((movie) => movie.id === id);
-  if (movie) return res.json(movie);
-
-  res.status(404).json({ message: "Movie not found" });
-});
-
-app.post("/movies", (req, res) => {
-  const result = validateMovie(req.body);
-
-  if (!result.success) {
-    return res.status(422).json({ error: JSON.parse(result.error.message) });
-  }
-
-  const newMovie = {
-    id: randomUUID(),
-    ...result.data,
-  };
-
-  movies.push(newMovie);
-
-  res.status(201).json(newMovie);
-});
-
-app.delete("/movies/:id", (req, res) => {
-  const id = req.params.id;
-  const movieIndex = movies.findIndex((movie) => movie.id === id);
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: "Movie not found" });
-  }
-
-  movies.splice(movieIndex, 1);
-  return res.json({ message: "Movie deleted" });
-});
-
-app.patch("/movies/:id", (req, res) => {
-  const { id } = req.params;
-  const result = validatePartialMovie(req.body);
-
-  if (!result.success) {
-    return res.status(422).json({ error: JSON.parse(result.error.message) });
-  }
-
-  const movieIndex = findIndex((movie) => movie.id === id);
-
-  if (movieIndex === -1) {
-    return res.status(404).json({ message: "Movie not found" });
-  }
-
-  const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data,
-  };
-
-  return res.json(updateMovie);
-});
-
+app.use("/movies", moviesRouter);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
