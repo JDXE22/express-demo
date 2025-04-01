bdcc869267eee2fbe3c9ad5b16c3fd7de02df745
David <davidesparzaj22@gmail.com>
2025-04-01 15:30:52 -0500

    chore(express-demo/api-rest): create middlewares/cors.js and integrate in app.js

--- API-REST/app.js
+++ API-REST/app.js
@@ -1,32 +1,11 @@
 import express, { json } from "express";
-import cors from "cors";
 import { moviesRouter } from "./routes/moviesRouter.js";
+import { corsMiddleware } from "./middlewares/cors.js";
 moviesRouter
 
 const app = express();
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      const ACCEPTED_ORIGINS = [
-        "http://localhost:8080",
-        "http://localhost:3000",
-        "https://movies.com",
-        "https://localhost:1",
-      ];
-
-      if (ACCEPTED_ORIGINS.includes(origin)) {
-        return callback(null, true);
-      }
-
-      if (!origin) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
-  })
-);
+app.use(corsMiddleware);
 
 app.use(json());
 app.disable("x-powered-by");
